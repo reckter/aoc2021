a0333ce3ad4e5d6d01f7d3e976a9697dadf7a8d6
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2021-12-01 08:55:06 +0100

    :sparkles: Solve Day 1

---
+++ main/days/Day1.kt
@@ -0,0 +1,35 @@
+package me.reckter.aoc.days
+
+import me.reckter.aoc.Day
+import me.reckter.aoc.solution
+import me.reckter.aoc.solve
+import me.reckter.aoc.toIntegers
+
+class Day1 : Day {
+    override val day = 1
+
+    override fun solvePart1() {
+        loadInput()
+            .toIntegers()
+            .zipWithNext()
+            .map { (a, b) -> b - a }
+            .count { it > 0 }
+            .solution(1)
+    }
+
+    override fun solvePart2() {
+        loadInput()
+            .toIntegers()
+            .asSequence()
+            .windowed(3,1)
+            .map { it.sum() }
+            .zipWithNext()
+
+            .map { (a,b) -> b - a }
+            .count { it > 0}
+            .solution(2)
+
+    }
+}
+
+fun main() = solve<Day1>()
