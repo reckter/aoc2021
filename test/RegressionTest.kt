00562afa5491f64ac0aaf1bb4e3421ce1e3ce0a1
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2022-01-15 00:20:48 +0100

    :bug:

--- test/RegressionTest.kt
+++ test/RegressionTest.kt
@@ -1,6 +1,7 @@
 package me.reckter.aoc
 
 import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import org.reflections.Reflections
